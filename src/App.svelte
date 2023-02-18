fc0e7de238bbff7d4bc1b64a558c4e096f403095
dedi c <dedi.10118901@mahasiswa.unikom.ac.id>
2023-02-18 19:43:25 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+// @ts-nocheck
+
+</script>
+
+<div class="flex h-screen items-center justify-center safe-top safe-left safe-right safe-bottom disable-scrollbars select-none">
+  <h1 class="font-bold">Kertas Putih</h1>
+</div>
+<style>
+
+</style>
