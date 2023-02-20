81809a64df5f1f70882b00efed80db9c72d6b674
dedi c <dedi.10118901@mahasiswa.unikom.ac.id>
2023-02-20 18:18:23 +0700

    add flex col

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@ const handleRequestPermission = () => {
 }
 </script>
 
-<div class="flex h-screen items-center justify-center safe-top safe-left safe-right safe-bottom disable-scrollbars select-none">
+<div class="flex flex-col h-screen items-center justify-center safe-top safe-left safe-right safe-bottom disable-scrollbars select-none">
   <button class="rounded-full px-2 py-1 bg-emerald-500 text-white font-bold" on:click={() => handleRequestPermission()}>Aktifkan</button>
   <p>token : {token}</p>
 </div>
